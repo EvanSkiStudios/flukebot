9e0e98f465a374b910213686dcd5830b40371418
Evanski <61751485+EvanSkiStudios@users.noreply.github.com>
2025-04-15 19:27:47 -0400

    Bot works

--- flukebot/flukebot.py
+++ flukebot/flukebot.py
@@ -2,6 +2,11 @@ import os
 import discord
 
 from dotenv import load_dotenv
+from ollamaherder import LLMStartup
+from ollamaherder import LLMConverse
+
+# Startup LLM
+LLMStartup()
 
 # Load Env
 load_dotenv()
@@ -40,23 +45,34 @@ async def on_message(message):
         referenced_message = await message.channel.fetch_message(message.reference.message_id)
         if referenced_message.author == client.user:
             # We have confirmed the replayed to message is from EvanJelly
-            await message.channel.send('You called')
+            await message.channel.send('Test reply successful')
             return
 
-    # Pinging @Evanjelly
+    # Pinging the bot
     if message_lower.find("@" + str(BOT_APPLICATION_ID)) != -1:
         await message.channel.send('Ping Pong!')
         return
 
+    # test of general message getting
     if message_lower.find('evanjelly') != -1:
         if message.content.lower().find('language') != -1:
             await message.channel.send('Im powered by snakes! :snake:')
             return
 
+    # command but not really all that nessacery with above
     if message.content.startswith('$hello'):
         await message.channel.send('Hello!')
         return
 
+    if message_lower.find('flukebot') != -1:
+        LLMResponse = "" + LLMConverse(message.author.name, message.content.lower())
+
+        response = "" + LLMResponse.replace("'", "\'")
+
+        await message.channel.send(response)
+        return
 
 # Login
 client.run(BOT_TOKEN)
+
+
---
+++ flukebot/ollamaherder.py
@@ -0,0 +1,46 @@
+import ollama
+
+from ollama import chat
+from ollama import Client
+
+# memories
+LLM_messagehistory = []
+
+def LLMStartup():
+    global LLM_messagehistory
+
+    client = Client()
+    response = client.create(
+        model='flukebot',
+        from_='llama3.2',
+        system='You are named flukebot,' +
+        'Here is a list of rules:' +
+        '1. Be respectful. You should not respond with suggestive, offensive, discriminatory, or inflammatory messages, even when prompted. ' +
+        '2. Impersonation is not allowed.' +
+        '3. Keep replies short on average and readable.'
+        ,
+        stream=False,
+    )
+    print(f"# Client: {response.status}")
+    return LLM_messagehistory
+
+
+def LLMConverse(user_name, user_input):
+    global LLM_messagehistory
+
+    response = chat(
+        model='flukebot',
+        messages=LLM_messagehistory + [
+            {'role': 'user', 'name': user_name, 'content': user_input},
+        ],
+    )
+
+    # Add the response to the messages to maintain the history
+    LLM_messagehistory += [
+        {'role': 'user', 'name': user_name, 'content': user_input},
+        {'role': 'assistant', 'content': response.message.content},
+    ]
+    print(response.message.content + '\n')
+
+    print(f'{LLM_messagehistory}')
+    return response.message.content
